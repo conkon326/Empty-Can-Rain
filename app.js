86ddad793872f70633be0ba446a475b1cd06e37a
con <128914799+conkon326@users.noreply.github.com>
2023-04-01 00:26:43 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+document.getElementById('start-btn').addEventListener('click', () => {
+  // Remove all empty cans from the canvas
+  emptyCans.forEach(emptyCan => canvas.remove(emptyCan.object));
+  emptyCans.length = 0;
+
+  // Create empty cans based on the input value
+  const numCans = document.getElementById('num-cans').value;
+  for (let i = 0; i < numCans; i++) {
+    const emptyCan = new EmptyCan();
+    emptyCans.push(emptyCan);
+  }
+
+  // Start animation loop
+  function animate() {
+    // Move all the empty cans down the screen
+    emptyCans.forEach(emptyCan => {
+      emptyCan.move();
+      emptyCan.checkIfOffScreen();
+    });
+
+    // Request next animation frame
+    requestAnimationFrame(animate);
+  }
+
+  animate();
+  
+  // Update the number of cans based on the input value
+  document.getElementById('num-cans').addEventListener('input', () => {
+    const numCans = document.getElementById('num-cans').value;
+    // Remove extra empty cans
+    while (emptyCans.length > numCans) {
+      const emptyCan = emptyCans.pop();
+      canvas.remove(emptyCan.object);
+    }
+    // Add new empty cans
+    for (let i = emptyCans.length; i < numCans; i++) {
+      const emptyCan = new EmptyCan();
+      emptyCans.push(emptyCan);
+    }
+  });
+});
